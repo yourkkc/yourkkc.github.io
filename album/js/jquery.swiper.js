881fb058e543714da2fbf4437569e7aaf348cc79
yourkkc <qc_2e975fa3328f4f9d89fbac65e8acf802@git.code.tencent.com>
2019-06-19 15:00:43 +0800

    相册

---
+++ album/js/jquery.swiper.js
@@ -0,0 +1,37 @@
+(function($) {      
+    $.fn.swiper = function(options) {   
+    	this.pagesId = options.pagesId;
+    	this.transition = ['slide' ,'rotate' , 'flip' , 'card' , 'fade'];
+    	this.list = [];
+    	this.swiper = null;
+    	this.init = function(){
+    		
+    		for(var i in this.pagesId){
+    			let i1 = i;
+    			var direction = $("#"+this.pagesId[i1]).attr("data-t-direction");
+    			var duration = $("#"+this.pagesId[i1]).attr("data-t-duration");
+    			var name = $("#"+this.pagesId[i1]).attr("data-t-name");
+    			
+    			if(name=='random'){
+    				name = this.transition[Math.floor(Math.random()*this.transition.length)];
+    			}
+    			
+    			var content = $("#"+this.pagesId[i1]).html();
+    			this.list.push({
+    				content :content,
+    				transition : {
+    					name : name,
+    					duration :duration,
+    					direction:direction
+    				}
+    			});
+    		}
+    		 this.swiper = new Swiper({
+    				container : this[0],
+    				data : this.list,
+    				isLoop : true,
+    			});
+    	}
+    	this.init();
+    };     
+})(jQuery); 
